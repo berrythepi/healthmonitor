13d7c7da4cbff0b9d48cf5afb369cd3eccb0b6f9
berrythepi <61709824+berrythepi@users.noreply.github.com>
2020-05-10 21:19:38 -0400

    Add files via upload

---
+++ healthmonitor.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#network stats
+iostat
+
+#who is logged in
+w vivek
+
+#how long has the server been up
+uptime
+
+#memory in use and available
+free
+
+#real time server usage
+sar 4 5
+
+
